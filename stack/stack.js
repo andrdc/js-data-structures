51e654394d7c340e4bf969c753e172478482ea05
Andres Ruiz <andrsruiz@gmail.com>
2020-07-25 13:00:14 -0500

    Add WeakMap to set private attributes

--- stack/stack.js
+++ stack/stack.js
@@ -1,55 +1,70 @@
-class Stack{
-	constructor(){
-		this.items = [];
-	}
+/* With this approach using a WeakMap to have private attributes
+ * we can not inheritance the same way as any other normal ES6 class.
+ * But we make sure that the Stack object is used as intented. In
+ * this particular case, we don't want to extent our class to be
+ * inheritance by other obect. */
 
-	/* Get size of the stack
-	 * @param none : none
-	 * @return length : int */
-	size(){
-		return this.items.length;
-	}
-
-	/* Check Stack emptiness
-	 * @param none : none
-	 * @return empty : boolean */
-	isEmpty(){
-		return this.size() === 0;
-	}
-
-	/* Add new element to the Stack. This means that the new element
-	 * is going to the be at top of all of the other elements.
-	 * @param element : any
-	 * @return items : array */
-	push(element){
-		this.items.push(element);
-		return this.items;
-	}
-
-	/* Remove an element from the Stack. The top element is going to
-	 * be pop out the Stack.
-	 * @param none : none
-	 * @return element : any */
-	pop(){
-		return this.items.pop();
-	}
-
-	/* Return top element of the Stack
-	 * @param none : none
-	 * @return element : any */
-	peek(){
-		return this.items[this.size() - 1];
-	}
-
-	/* Clear out the Stack. Looping until the size is 0
-	 * and pop one element by iteration.
-	 * @param none : none
-	 * @return elements : array */
-	clear(){
-		while(this.size() > 0){
-			this.pop();
+let Stack = (function() {
+	const items = new WeakMap();
+	class Stack{
+		constructor(){
+			items.set(this, []);
 		}
-		return this.items;
+
+		/* Get size of the stack
+		 * @param none : none
+		 * @return length : int */
+		size(){
+			let myItems = items.get(this);
+			return myItems.length;
+		}
+
+		/* Check Stack emptiness
+		 * @param none : none
+		 * @return empty : boolean */
+		isEmpty(){
+			return this.size() === 0;
+		}
+
+		/* Add new element to the Stack. This means that the new element
+		 * is going to the be at top of all of the other elements.
+		 * @param element : any
+		 * @return items : array */
+		push(element){
+			let myItems = items.get(this);
+			myItems.push(element);
+			return myItems;
+		}
+
+		/* Remove an element from the Stack. The top element is going to
+		 * be pop out the Stack.
+		 * @param none : none
+		 * @return element : any */
+		pop(){
+			let myItems = items.get(this);
+			return myItems.pop();
+		}
+
+		/* Return top element of the Stack
+		 * @param none : none
+		 * @return element : any */
+		peek(){
+			let myItems = items.get(this);
+			return myItems[this.size() - 1];
+		}
+
+		/* Clear out the Stack. Looping until the size is 0
+		 * and pop one element by iteration.
+		 * @param none : none
+		 * @return elements : array */
+		clear(){
+			let myItems = items.get(this);
+			while(this.size() > 0){
+				this.pop();
+			}
+			return myItems;
+		}
 	}
-}
+	return Stack;
+})();
 module.exports = Stack;
