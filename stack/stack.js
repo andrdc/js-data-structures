41e9234e06bc4ea1194d1917b60329bd1602e7f8
Andres Ruiz <andrsruiz@gmail.com>
2020-07-25 11:29:09 -0500

    Test Stack's push method

--- stack/stack.js
+++ stack/stack.js
@@ -9,5 +9,14 @@ class Stack{
 	size(){
 		return this.items.length;
 	}
+
+	/* Add new element to the Stack. This means that the element is
+	 * going to the be at top of all of the other elements.
+	 * @param element : any
+	 * @return items : array */
+	push(element){
+		this.items.push(element);
+		return this.items;
+	}
 }
 module.exports = Stack;
--- tests/unit/stack.spec.js
+++ tests/unit/stack.spec.js
@@ -3,7 +3,15 @@ const Stack = require('../../stack/stack');
 describe("size()", () => {
 	var myStack = new Stack();
 
-	it("Check size for an empty Stack", () => {
+	it("Size of an empty Stack", () => {
 		expect(myStack.size()).toBe(0);
+	});
+});
+
+describe("push(element)", () => {
+	var myStack = new Stack();
+
+	it("Add new element to a Stack", () => {
+		expect(myStack.push('Andres')).toStrictEqual(['Andres']);
 	});
 });
