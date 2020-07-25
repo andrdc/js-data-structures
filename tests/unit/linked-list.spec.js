bff59682765f7e8bf17a8621bea85330b2205c24
Andres Ruiz <andrsruiz@gmail.com>
2020-07-25 11:20:30 -0500

    Change test to it

--- tests/unit/linked-list.spec.js
+++ tests/unit/linked-list.spec.js
@@ -11,23 +11,23 @@ describe("Basic methods : size(), isEmpty(), getHead(), indexOf(item)", () => {
 	let node = new Node("A1");
 	myLinkedList2.append("A1");
 
-	test("Check size", () => {
+	it("Check size", () => {
 		expect(myLinkedList.size()).toBe(3);
 	});
 
-	test("Check emptiness", () => {
+	it("Check emptiness", () => {
 		expect(myLinkedList.isEmpty()).toBeFalsy();
 	});
 
-	test("Return list's head", () => {
+	it("Return list's head", () => {
 		expect(myLinkedList2.getHead()).toMatchObject(node);
 	});
 
-	test("Return item's index", () => {
+	it("Return item's index", () => {
 		expect(myLinkedList.indexOf("G5")).toBe(1);
 	});
 
-	test("Item is not in the list therefore there's no index", () => {
+	it("Item is not in the list therefore there's no index", () => {
 		expect(myLinkedList.indexOf("B4")).toBe(-1);
 	});
 });
@@ -35,11 +35,11 @@ describe("Basic methods : size(), isEmpty(), getHead(), indexOf(item)", () => {
 describe("append()", () => {
 	let myLinkedList = new LinkedList();
 
-	test("Insert first node", () => {
+	it("Insert first node", () => {
 		expect(myLinkedList.append("A1")).toMatch("A1");
 	});
 
-	test("Insert another node", () => {
+	it("Insert another node", () => {
 		expect(myLinkedList.append("G5")).toMatch("A1, G5");
 	});
 });
@@ -50,15 +50,15 @@ describe("removeAt(position)", () => {
 	myLinkedList.append("G5");
 	myLinkedList.append("F4");
 
-	test("Remove item that is not the head", () => {
+	it("Remove item that is not the head", () => {
 		expect(myLinkedList.removeAt(1)).toMatch("A1, F4");
 	});
 
-	test("Try remove at invalid position", () => {
+	it("Try remove at invalid position", () => {
 		expect(myLinkedList.removeAt(5)).toBeFalsy();
 	});
 
-	test("Remove Head", () => {
+	it("Remove Head", () => {
 		expect(myLinkedList.removeAt(0)).toMatch("F4");
 	});
 });
@@ -69,7 +69,7 @@ describe("remove(item)", () => {
 	myLinkedList.append("G5");
 	myLinkedList.append("F4");
 
-	test("Remove item", () => {
+	it("Remove item", () => {
 		expect(myLinkedList.remove("G5")).toMatch("A1, F4");
 	});
 });
@@ -79,15 +79,15 @@ describe("insert(position, item)", () => {
 	myLinkedList.append("A1");
 	myLinkedList.append("F4");
 
-	test("Check out of bounds values", () => {
+	it("Check out of bounds values", () => {
 		expect(myLinkedList.insert(6, "G5")).toBeFalsy();
 	});
 
-	test("Insert in the head", () => {
+	it("Insert in the head", () => {
 		expect(myLinkedList.insert(0, "G5")).toMatch("G5, A1, F4");
 	});
 
-	test("Insert in another position", () => {
+	it("Insert in another position", () => {
 		expect(myLinkedList.insert(1, "D2")).toMatch("G5, D2, A1, F4");
 	});
 });
@@ -98,7 +98,7 @@ describe("toString()", () => {
 	myLinkedList.append("G5");
 	myLinkedList.append("F4");
 
-	test("Return list as string", () => {
+	it("Return list as string", () => {
 		expect(myLinkedList.toString()).toMatch("A1, G5, F4");
 	});
 });
