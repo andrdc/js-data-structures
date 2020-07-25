809703fc4d43b677235dfb54e92485ced2d03e26
Andres Ruiz <andrsruiz@gmail.com>
2020-07-25 11:30:53 -0500

    Add test for non-empty Stack in size method

--- tests/unit/stack.spec.js
+++ tests/unit/stack.spec.js
@@ -6,6 +6,12 @@ describe("size()", () => {
 	it("Size of an empty Stack", () => {
 		expect(myStack.size()).toBe(0);
 	});
+
+	it("Size of an non-empty Stack", () => {
+		myStack.push('Andres');
+		myStack.push('Arturo');
+		expect(myStack.size()).toBe(2);
+	});
 });
 
 describe("push(element)", () => {
