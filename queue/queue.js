7a89da29724cf6ddcda001b4f005081dd8b2ae21
Andres Ruiz <andrsruiz@gmail.com>
2020-08-10 16:29:12 -0500

    Add isEmpty() and enqueue() methods with Tests

--- queue/queue.js
+++ queue/queue.js
@@ -12,6 +12,23 @@ let Queue = (function() {
 			let myItems = items.get(this);
 			return myItems.length;
 		}
+
+		/* Check wether or not the queue is empty
+		 * @param none : none
+		 * @return isEmpty : bool */
+		isEmpty(){
+			let myItems = items.get(this);
+			return this.size() === 0 ? true : false;
+		}
+
+		/* Add new item to the queue
+		 * @param item : Any
+		 * @return items : Array */
+		enqueue(item){
+			let myItems = items.get(this);
+			myItems.push(item);
+			return myItems;
+		}
 	}
 	return Queue;
 })();
--- tests/unit/queue.spec.js
+++ tests/unit/queue.spec.js
@@ -1,9 +1,26 @@
 const Queue = require('../../queue/queue');
 
-describe("size()", () => {
+describe('size()', () => {
 	var myQueue = new Queue();
 
-	it("Size of an empty Queue", () => {
+	it('Size of an empty Queue', () => {
 		expect(myQueue.size()).toBe(0);
+	});
+});
+
+describe('isEmpty()', () => {
+	var myQueue = new Queue();
+
+	it('Check size of empty Queue', () => {
+		expect(myQueue.isEmpty()).toBeTruthy();
+	});
+});
+
+describe('enqueue(item)', () => {
+	var myQueue = new Queue();
+
+	it('Add new item to the Queue', () => {
+		expect(myQueue.enqueue('Tyra')).toStrictEqual(['Tyra']);
+		expect(myQueue.enqueue('Coqueta')).toStrictEqual(['Tyra', 'Coqueta']);
 	});
 });
